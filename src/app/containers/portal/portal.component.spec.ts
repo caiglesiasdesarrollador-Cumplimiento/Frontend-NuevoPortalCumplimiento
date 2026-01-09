c0766daf6b01988c5e1cf448eff7d5cb319a6939
Carlos Andres Iglesias Arrieta <caiglesias@personalsoft.com>
2026-01-09 16:30:57 -0500

    fix: corregir typo maimport -> import en portal.component.spec.ts

--- src/app/containers/portal/portal.component.spec.ts
+++ src/app/containers/portal/portal.component.spec.ts
@@ -1,4 +1,4 @@
-maimport { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PortalComponent } from './portal.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
