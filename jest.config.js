d4dfadbffe3b9ab3e1e23208bdf02b91b13bd9cd
Daniel Camacho Fonseca <dacamacho@personalsoft.com>
2026-01-13 16:24:09 -0500

    GD981-760 - Se agregan cambios sobre componentes standalone.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '@app/(.*)': '<rootDir>/src/app/$1',
     '@environment': '<rootDir>/src/environments/environment',
     '@shared/(.*)': '<rootDir>/src/app/shared/$1',
+    '^tech-block-lib$': '<rootDir>/src/app/lib/index.ts',
   },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
