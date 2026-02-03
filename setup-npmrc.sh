f2024f277da4a254e2a6af450a709adc51a6bc85
Carlos Andres Iglesias Arrieta <caiglesias@personalsoft.com>
2026-02-03 13:23:17 -0500

    fix: Script npmrc con token por defecto para Vercel

--- setup-npmrc.sh
+++ setup-npmrc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "registry=https://segurosbolivar.jfrog.io/artifactory/api/npm/bolivar-npm-core-web-kit-components-lib-prod-virtual/" > .npmrc
-echo "//segurosbolivar.jfrog.io/artifactory/api/npm/bolivar-npm-core-web-kit-components-lib-prod-virtual/:_authToken=$JFROG_AUTH_TOKEN" >> .npmrc
+# Usar variable de entorno si existe, sino usar token por defecto
+TOKEN=${JFROG_AUTH_TOKEN:-cmVmdGtuOjAxOjE3OTYzMzM4MTI6ODAyZ25yUXV2YmkyZEdMVXJxNk9uenR0cHZz}
+echo "//segurosbolivar.jfrog.io/artifactory/api/npm/bolivar-npm-core-web-kit-components-lib-prod-virtual/:_auth=\"$TOKEN\"" >> .npmrc
 echo "always-auth=true" >> .npmrc
